1f650e8f091d3b578ccb14fd42c2daca80e45093
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-23 18:12:58 +0100

    :art: Basic: Question1 done   :art:

--- Basic/question1.js
+++ Basic/question1.js
@@ -1,2 +1,34 @@
 /* Given an array ‘arr’ of size ‘n’ find the largest element in the array. */
 
+function largestElementInArray(arr,n) {
+    let max = 0;
+    n = arr.length;
+    for(let i= 0; i<arr.length; i++) {
+        if(arr[i]>max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+console.log(largestElementInArray([22,4,10], 3));
+
+
+//Autre solution
+
+const largestElement = (arr,n) => {
+    return arr.reduce((acc, cur) =>{
+        return acc > cur ? acc : cur;
+    },arr[0])
+}
+console.log(largestElement([19,42,1], 3));
+
+
+// Autre solution
+
+function largestElementArray(arr,n) {   
+    n= arr.length;
+    arr.sort((a,b) => a-b)
+    return (arr[arr.length-1]);
+
+}
+console.log(largestElementArray([19,40,1], 3));
