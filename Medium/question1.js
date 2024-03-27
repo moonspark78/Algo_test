4742933e5583288ac013cf330ac266e905e75865
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-27 23:58:30 +0100

    :art: Medium: Question1  Start   :art:

---
+++ Medium/question1.js
@@ -0,0 +1,12 @@
+/* Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+Exemples :
+
+[1,1,1] et k=2 => output = 2
+[1,2,3,0,3] et k=3 => output = 5
+[4, 2, 9, 7, 19] et k=5 => output = 0
+[1, -1, 2] et k=2 ⇒ output = 2
+[1,1,1,-1] et k=2 => output = 3 */
+
+function subArraySum(arr, k){
+    
+}
