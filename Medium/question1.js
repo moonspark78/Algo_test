183dacc4099dd05c993bbde002b32546dd704a9b
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-28 00:07:18 +0100

    :art: Medium: Question1  Done   :art:

--- Medium/question1.js
+++ Medium/question1.js
@@ -8,5 +8,17 @@ Exemples :
 [1,1,1,-1] et k=2 => output = 3 */
 
 function subArraySum(arr, k){
-    
+    let count = 0;
+    for (let i = 0; i < arr.length; i++){
+        let sum = 0;
+        for(let j = i; j < arr.length; j++){
+            sum += arr[j]
+            if(sum === k){
+                count++;
+            }
+        }
+    }
+    return count;
 }
+
+console.log(subArraySum([1,2,3,0,3],3));
